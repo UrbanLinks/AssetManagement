c2eaed48273603ed2527238ec08ba940672167eb
rebecca-dziedzic <74367705+rebecca-dziedzic@users.noreply.github.com>
2024-07-26 16:44:13 -0400

    changing home layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,8 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+// Import the Prism themes
+const { themes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
